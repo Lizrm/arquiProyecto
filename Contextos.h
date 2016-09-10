fa2776d4d5edb23a6b1c025bda08a0db4525cc19
Lizbeth <lisbeth.8.12@gmail.com>
2016-09-10 01:55:14 +0000

    Completacion clase contextos

--- Contextos.h
+++ Contextos.h
@@ -3,30 +3,65 @@
 
 class Contextos
 {
-
     struct contexto
     {
     	int pc;     //
     	int  regist[33];//Semaforo asociado al Thread
-    	contexto * sig;
+    	contexto * siguiente;
     };
 
 public:
 	
-	Contextos(int hilillos){} //Constructor de la clase
+	//Constructor de la clase
+	Contextos(int hilillos)
+	{
+	    n = 0;
+	} 
 	
 	~Contextos(){}	//Destructor de la clase
 
-	void Agregar(int p, int *reg)             //Guarda el contexto
+    //Guarda el contexto
+    //reg se debe recibir por referencia creo
+	void Agregar(int p, int *reg)            
 	{
 	    
+	    contexto * nueva  = new contexto;
+	    nueva->pc = p;
+	    for(int i = 1; i <33; ++i)
+	    {
+	        nueva->regist[i] = reg[i];
+	    }
+	    ultimo -> siguiente = nueva;
+	    ultimo = nueva;
+	    
+	    if(n == 0)
+	    {
+	        primero = nueva;
+	    }
+	     n++;
 	}
-	bool Sacar();		//Borra el contexto 
+	
+	//Borra el contexto 
+	//estos se deben recibir por referencia &
+	void Sacar(int p, int*reg)
+	{
+	    p = primero->pc;
+	    for(int i = 1; i <33; ++i)
+	    {
+	        reg[i] = primero->regist[i];
+	    }
+	    
+        primero = primero->siguiente;
+	    contexto * aux = primero;
+	    delete aux;
+	    --n;
+	}
 
 private:
 
     contexto * primero;
     contexto * ultimo;
+    int n;
 };
 
 #endif //CONTEXTOS_H
