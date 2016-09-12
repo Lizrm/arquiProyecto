7f86adc75ed12b69d571e38e203130b9b4de5ab0
Lizbeth <lisbeth.8.12@gmail.com>
2016-09-12 02:11:23 +0000

    copie lo adriana

--- Procesador.cc
+++ Procesador.cc
@@ -1,18 +1,30 @@
+// Compilar usando g++ -pthread filename1 [filename2...] [-o outputFileName]
+/**
+ * Tarea Programada: simulacion de MIPS 3 nucleos
+ * Adriana Mora Calvo        B24385
+ * Lisbeth Rojas Montero     B15745
+ * */
 #include <iostream>
+#include <pthread.h> //Biblioteca para uso de pthreads
 #include "Contextos.h"
 using namespace std;
 
 int main() 
 {
-    cout << "Hello World!" << endl;
     
     //variables compartidas
-    
-    int * memDatos;
-    int * memInstruc;
-    int busDatos, busInstruc;
-    Contextos *cola;
-    int total;
+    pthread_barrier_t barrera;  // inicializacion de la barrera
+
+    int * memDatos;             // cache de datos (cada nucleo tiene una propia)
+    int * memInstruc;           // cache de instrucciones (cada nucleo tiene una propia)
+    int busDatos, busInstruc;   // para lectura y escritura deben esperar el bus
+    Contextos *cola;            // para poder cambiar de contexto entre hilillos
+    int total, cop, rf1, rf2, rd;
+    //total     total???? es para el quantum?? 
+    // cop codigo de operacion
+    // rf1 registro fuente
+    // rf2 registro fuente 2 o registro destino dependiendo de la instruccion
+    // rd registro destino o inmediato dependiendo de la instruccion
     
     memDatos = new int[96];
     memInstruc = new int [640];
@@ -31,16 +43,19 @@ int main()
 
 void Nucleos()
 {
-    int * reg = new int[33];    //Registro 0  = reg[0], registro RL = reg[32]
-    int cacheDatos[4][6];       //4 columnas 5 filas, (fila 0 p0, fila 3 etiqueta, fila 4 valides)
-    int cacheInstruc[4][6];     //4 columnas 5 filas, (fila 0 p0, fila 3 etiqueta, fila 4 valides)
-    int PC;
+    int * reg = new int[33];    //Registro 0 = reg[0] (contiene un cero), registro RL = reg[32]         LOS PROCESOS COMPARTEN LOS REGISTROS ASI QUE NO SE SI ESTO DEBE DE IR ARRIBA
+    int cacheDatos[4][6];       //4 columnas 5 filas, (fila 0 p0, fila 2 p1, fila 3 etiqueta, fila 4 valides)
+    int cacheInstruc[4][6];     //4 columnas 5 filas, (fila 0 p0, fila 2 p1, fila 3 etiqueta, fila 4 valides)
+    int PC;                     // para el control de las instrucciones
     int dirDatos, dirInstruc;
     
     int bloque, fisica, quantum;
+    // bloque es el bloque de memoria cache
+    //fisica es
+    //quatum es el tiempo dado por el usuario
     
-    dirDatos = 0;//indice de la cache
-    dirInstruc = 0;//indice de la cache
+    dirDatos = 0;       //indice de la cache
+    dirInstruc = 0;     //indice de la cache
     reg[0] = 0;
     for(int i = 0; i < 4; ++i)
     {
@@ -52,6 +67,9 @@ void Nucleos()
     }
 
     bool continuar;    
+    
+    // Modificación del quatum y ciclos de reloj 
+    // Realizado por el hilo 0 que se encarga de la coordinación de los demás hilos
     while(continuar)
     {
         //cargar PC;
@@ -67,15 +85,18 @@ void Nucleos()
     }
 }
 
+//Calcular direccion fisica de datos
 int CalcularFisica(int direccion)
 {
-    if(direccion >= 384)
+    if(direccion >= 384) //DE DONDE SE SACO ESTE 384?????
     {
         return (direccion - 384);
     }
     return (direccion / 4);
 }
 
+//Fallo de Cache
+// Cuando ocurre fallo de cache debo de traer el bloque completo de donde esta lo que quiero modificar
 void SubirBLoqueDatos(int columna, int posicion, bool datos)
 {
     if(datos)
@@ -99,10 +120,77 @@ void SubirBLoqueDatos(int columna, int posicion, bool datos)
         cacheInstruc[columna][5] = 1;
     }
     int pasado = 0;
-    while(pasado < 28)
+    while(pasado < 28) //Simulacion de que un fallo de cache dura 28 ciclos de reloj
     {
-        //doy ciclo
+        //tic de reloj
         ++pasado;
     }
     
+}
+
+void Ejecutar(int cop){ //recibe el codigo de operacion para saber que hacer con los operandos
+    
+    //Codificacion de las instrucciones recibidas
+    switch(cop) { //cop es el codigo de operacion que tenemos que definir como se lee la instruccion para meter eso en una variable entera
+        case 8 : //DADDI 
+    //      rf1 <------- rf2 + inm
+        reg[rf1] =  reg[rf2] + rd;
+        
+        case 32 : //DADD
+    //      rd <------ rf1 + rf2
+        reg[rd] = reg[rf1] + reg[rf2];
+        
+        case 34 : //DSUB
+    //     rd <------- rf1 - rf2
+        reg[rd] = reg[rf1] - reg[rf2];
+        
+        case 12: //DMUL
+    //      rd <------ rf1 * rf2
+        reg[rd] = reg[rf1] * reg[rf2];
+        
+        case 14: //DIV
+    //      rd <------ rf1 * rf2
+        reg[rd] = reg[rf1] / reg[rf2];
+        
+        case 4 : //BEZ
+    // si rf = 0 entonces SALTA
+        if(reg[rf1] == 0){
+            //que hago con la n que recibo en rd
+        }
+        
+        case 5 : //BNEZ
+    // si rf z 0 o rf > 0 entonces SALTA
+        if(reg[rf1] < 0 || reg[rf1] > 0){
+             //que hago con la n que recibo en rd
+        }
+        
+        case 3 : //JAL
+    //   reg 31 = PC
+        reg[31] = PC;
+    //  PC = PC + inm;
+        PC = PC + rd;
+        
+        case 2 :  //JR
+    //  PC = rf1
+        PC = reg[rf1];
+        
+        case 50 : //LL
+        //Se implementará en la tercera entrega
+        
+        case 51 : //SC
+        //Se implementará en la tercera entrega
+        
+        case 35 : //LW
+        //  ESTO DEBEMOS DE VER COMO LO VAMOS A PROGRAMAR
+        //PORQUE HAY QUE VER LO DE LA TRADUCCION A BLOQUE Y PALABRA EN CACHE
+        // Y FALLOS DE CACHE
+        //USANDO EL BUS DE DATOS 
+        //PROGRAMAR LO DE LOS SEMAFOROS PARA EL USO DE BUSES
+        case 43: //SW
+        //ESTE SE DEBE DE PROGRAMAR ESCRIBIENDO EN MI CACHE Y EN LA MEMORIA PRINCIPAL
+        //USANDO EL BUS DE DATOS 
+        //PROGRAMAR LO DE LOS SEMAFOROS PARA EL USO DE BUSES
+        case 63 : //FIN
+    //Detener el programa
+    }
 }
